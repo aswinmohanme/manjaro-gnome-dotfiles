4e41f9b814bb08938d9f3a4e955f65420088c791
aswinmohanme <dev.aswinmohan@gmail.com>
2020-11-08 16:14:39 +0530

    Well track fish too

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+
+# DISABLE COLORS
+# functions -e set_color
+# function set_color; end
+
+# for color in (set | grep -Eo '^fish_color_[a-zA-Z0-9_]+')
+    # optional: save the color to be restored later
+    #set -g old_$color $$color
+#   set $color normal
+# end
